1cf710b048480263af552f5409f1aa2539171d09
Константин <cool.olo124@list.ru>
2023-08-04 21:09:40 +0400

    добавил фильтрацию наций

--- src/components/Headbook/HeadbookСiv5/HeadbookCiv5.jsx
+++ src/components/Headbook/HeadbookСiv5/HeadbookCiv5.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { Link, Routes, Route } from 'react-router-dom'
-import styles from './HeadbookCiv5.module.sass'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+import styles from './HeadbookCiv5.module.sass';
+import axios from 'axios';
 import HeadbookCiv5Item from './HeadbookCiv5Item/HeadbookCiv5Item';
 
 export default function () {
-  const [jsonData, setJsonData] = useState(null)
-  console.log('я тут')
+  const [input, setInput] = useState('');
+  const [jsonData, setJsonData] = useState(null);
+  
   useEffect(() => {
     const jsonFilePath = '/headbook/headbook_civilization5/allCivs.json';
     axios.get(jsonFilePath)
@@ -15,29 +16,34 @@ export default function () {
       })
       .catch(err => {
         console.error('Ошибка при запросе к JSON файлу', err);
-      }); // Закрываем круглую скобку здесь
+      });
   }, []);
   
   return (
-    <div className={styles.headbook}>
-      {jsonData ? (
+    <>
+      <input
+        className={`${styles.input} input`}
+        placeholder="Введите нацию..."
+        onChange={(event) => {
+          setInput(event.target.value);
+          console.log(input);
+        }}
+      />
+      <div className={styles.headbook}>
+        {jsonData ? (
           <div className={`text ${styles.nation}`}>
             {          
-              Object.entries(jsonData).map(nation => {
-                return(
-                    <HeadbookCiv5Item key={nation[0]} nation={nation}/> 
-                )
-              })
+              Object.entries(jsonData).map(nation => (
+                nation[0].toLowerCase().includes(input.toLowerCase()) ? (
+                  <HeadbookCiv5Item key={nation[0]} nation={nation}/> 
+                ) : null
+              ))
             }
-        </div>
-            
-            )
-        
-        :
-        (<p className='text'>Идет загрузка наций...</p>
-        )
-    }
-    </div>
-
-  )
+          </div>
+        ) : (
+          <p className='text'>Идет загрузка наций...</p>
+        )}
+      </div>
+    </>
+  );
 }
--- src/components/Headbook/HeadbookСiv5/HeadbookCiv5Item/HeadbookCiv5Item.jsx
+++ src/components/Headbook/HeadbookСiv5/HeadbookCiv5Item/HeadbookCiv5Item.jsx
@@ -14,8 +14,11 @@ export default function HeadbookCiv5Item({ nation }) {
   return (
     <div className={`text ${styles.item}`} onClick={() => { setToggle(!toggle) }}>
       <div className={styles.item__top}>
-        {nationName}
+        <p className='text'>{nationName}</p>
         <img className={`text ${styles.item__img}`} src={nationImgSource} alt="" />
+        <svg className={toggle ? styles.active : ''} width="50px" height="50px" viewBox="0 0 24 24" fill="#999" xmlns="http://www.w3.org/2000/svg">
+        <path d="M5.70711 9.71069C5.31658 10.1012 5.31658 10.7344 5.70711 11.1249L10.5993 16.0123C11.3805 16.7927 12.6463 16.7924 13.4271 16.0117L18.3174 11.1213C18.708 10.7308 18.708 10.0976 18.3174 9.70708C17.9269 9.31655 17.2937 9.31655 16.9032 9.70708L12.7176 13.8927C12.3271 14.2833 11.6939 14.2832 11.3034 13.8927L7.12132 9.71069C6.7308 9.32016 6.09763 9.32016 5.70711 9.71069Z" fill="#999"/>
+        </svg>
       </div>
       {toggle ? (
         <div className={styles.content}>
