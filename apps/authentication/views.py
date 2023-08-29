7bfd8bb29b2ad4fbed19a7cbfe0ea73d04d383cc
kirin <kirin3243@gmail.com>
2023-08-30 02:13:01 +0400

    блядство2

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import login, logout
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.authentication import SessionAuthentication
 from .serializers import UserSignUpSerializer, UserLogInSerializer, KeySerializer, KeySignUpSerializer
 from apps.passwordChange.permissions import NotAuthenticated
@@ -38,17 +38,14 @@ class ConfirmSignUpView(APIView):
         return Response(serializer.data,status=status.HTTP_200_OK)
 
 class SessionView(ViewSet):
-    authentication_classes = (NotAuthenticated,)
+    permission_classes = (AllowAny,)
     serializer_class = UserLogInSerializer
     def create(self,request):
-        if bool(not request.user.is_authenticated):
-            user = request.data
-            serializer = self.serializer_class(data=user)
-            serializer.is_valid(raise_exception=True)
-            login(request,serializer.validated_data)
-            dataToResponse = serializer.data
-            return Response(dataToResponse, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_403_FORBIDDEN)
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        login(request,serializer.validated_data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     def update(self, request):
         if bool(request.user and request.user.is_authenticated):
             logout(request)
