474c7475a855f48625e88383d680b16938bf8362
Константин <cool.olo124@list.ru>
2023-09-03 21:41:40 +0400

    docker + nginx

--- src/components/Friends/Friends.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Friends() {
-  return (
-    <div>Friends</div>
-  )
-}
--- src/store/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import authReducer from './reducers/isAuthSlice'
-
-const rootReducer = combineReducers({
-  authReducer,
-})
-
-export default rootReducer
