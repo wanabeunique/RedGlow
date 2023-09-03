f8ee9e417a70a6590a70f34a7949a85d6e5d753b
kirin <kirin3243@gmail.com>
2023-09-03 23:42:44 +0400

    fixes

--- app/src/api/getIsAuth.js
+++ app/src/api/getIsAuth.js
@@ -3,17 +3,19 @@ import { setIsAuth } from "../store/reducers/isAuthSlice";
 import Cookies from "js-cookie";
 
 export default async function getIsAuth(dispatch){
-  axios.defaults.headers.common['X-CSRFToken'] = Cookies.get('csrftoken')
+  axios.defaults.headers.common['csrftoken'] = Cookies.get('csrftoken')
   try{
     const response = await axios.put(
       `${import.meta.env.VITE_API_SERVER}/user/checker/`,
-      {withCredentials: true}
+      {},
+      {withCredentials: true,headers:{"X-CSRFTOKEN":Cookies.get('csrftoken')}}
     )
-    if (response.status == 200){
+    if (response.status == 202){
       dispatch(setIsAuth(true))
     }
   }
   catch(error){
+    console.log(Cookies.get('csrftoken'))
     console.log(error)
     dispatch(setIsAuth(false))
   }
