f3d51aee8f1db31fa82b0a5ddcc557dfd9beef73
kirin <kirin3243@gmail.com>
2024-01-25 11:35:37 +0400

    cnsumer friends fixed

--- backend/apps/friends/consumers.py
+++ backend/apps/friends/consumers.py
@@ -57,7 +57,7 @@ class FriendConsumer(AsyncJsonWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        if self.group_name:
+        if hasattr(self,"group_name"):
             await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     @database_sync_to_async
